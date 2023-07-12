948d147fb96a66a89c307454e3b5faea8c5030cc
marciovsc <marciovictorsc@gmail.com>
2023-07-11 21:40:47 -0300

    add @Entity

--- src/main/java/com/project/professor/allocation/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -1,10 +1,11 @@
 package com.project.professor.allocation.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class Professor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
