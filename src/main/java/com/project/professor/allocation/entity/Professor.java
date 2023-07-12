35a30701275ea445e902753199bfb7fa1804186c
marciovsc <marciovictorsc@gmail.com>
2023-07-11 21:33:10 -0300

    Add class professor

---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,36 @@
+package com.project.professor.allocation.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Professor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true, nullable=false,length=11)
+	private String cpf;
+	private String name;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
