29ccf469de49c59396ed07236336c8585c2d146e
beatrizfelix12 <beatrizfelix443@gmail.com>
2023-07-11 22:44:04 -0300

    add class course correção

--- src/main/java/com/project/professor/allocation/entity/Course.java
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -1,23 +1,30 @@
 package com.project.professor.allocation.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
-	private Long id;
-	private String name;
+    @Id
+    private Long id;
+    private String name;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
 	}
 
 }
