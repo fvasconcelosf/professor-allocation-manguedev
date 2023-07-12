3567614af3d075eb49d80068e3be9322322f282b
Felipe Ferreira de Vasconcelos <fvasconcelos-@hotmail.com>
2023-07-11 21:16:13 -0300

    add package entity + class Application

---
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -0,0 +1,51 @@
+package com.project.professor.allocation.entity;
+
+import java.sql.Time;
+import java.time.DayOfWeek;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Allocation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private DayOfWeek day;
+	private Time start;
+	private Time end;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DayOfWeek getDay() {
+		return day;
+	}
+
+	public void setDay(DayOfWeek day) {
+		this.day = day;
+	}
+
+	public Time getStart() {
+		return start;
+	}
+
+	public void setStart(Time start) {
+		this.start = start;
+	}
+
+	public Time getEnd() {
+		return end;
+	}
+
+	public void setEnd(Time end) {
+		this.end = end;
+	}
+
+}
