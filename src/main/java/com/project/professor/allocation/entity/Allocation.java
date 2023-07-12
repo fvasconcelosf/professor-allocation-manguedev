ee8a6cbdcb822cb2637b79114b7d4e1eb7d272df
murilo <mc.storto@gmail.com>
2023-07-12 00:38:46 -0300

    add da class department e add do mapeamento classe-tabela

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -2,50 +2,70 @@ package com.project.professor.allocation.entity;
 
 import java.sql.Time;
 import java.time.DayOfWeek;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 @Entity
+@Table(name = "Allocation")
 public class Allocation {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private DayOfWeek day;
-	private Time start;
-	private Time end;
+	
+	
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@Enumerated(EnumType.STRING)
+		@Column(name = "day", nullable = false) 
+		private DayOfWeek day;
+		
+		@Temporal(TemporalType.TIME)
+		@Column(name = "start", nullable = false) 
+		private Date start;
+		
+		@Temporal(TemporalType.TIME)
+		@Column(name = "end", nullable = false) 
+		private Date end;
 
-	public Long getId() {
-		return id;
-	}
+		public Long getId() {
+			return id;
+		}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+		public void setId(Long id) {
+			this.id = id;
+		}
 
-	public DayOfWeek getDay() {
-		return day;
-	}
+		public DayOfWeek getDay() {
+			return day;
+		}
 
-	public void setDay(DayOfWeek day) {
-		this.day = day;
-	}
+		public void setDay(DayOfWeek day) {
+			this.day = day;
+		}
 
-	public Time getStart() {
-		return start;
-	}
+		public Date getStart() {
+			return start;
+		}
 
-	public void setStart(Time start) {
-		this.start = start;
-	}
+		public void setStart(Time start) {
+			this.start = start;
+		}
 
-	public Time getEnd() {
-		return end;
-	}
+		public Date getEnd() {
+			return end;
+		}
 
-	public void setEnd(Time end) {
-		this.end = end;
-	}
-
+		public void setEnd(Time end) {
+			this.end = end;
+		}
 }
--- src/main/java/com/project/professor/allocation/entity/Course.java
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -1,30 +1,37 @@
 package com.project.professor.allocation.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Course")
 public class Course {
-    @Id
-    private Long id;
-    private String name;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+		
+	@Column(name = "names", unique = true, nullable = false)
+	private String name;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
