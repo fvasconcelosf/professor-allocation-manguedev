fc278573b7d4d9d38554c587962cffcb584c8511
Daniel Tompson <danieltompsonv@gmail.com>
2023-07-17 19:07:22 -0300

    Add Interface Tests in ProfessorRepositoryTest

--- src/test/java/com/project/professor/allocation/repository/ProfessorRespositoryTest.java
+++ src/test/java/com/project/professor/allocation/repository/ProfessorRespositoryTest.java
@@ -1,6 +1,7 @@
 package com.project.professor.allocation.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,20 +19,51 @@ import com.project.professor.allocation.entity.Professor;
 @TestPropertySource(locations = "classpath:application.properties")
 
 public class ProfessorRespositoryTest {
-		
-		@Autowired
-		ProfessorRepository professorRepository; 
-			
-		@Test 
-		public void findAll() {
-			List<Professor> professors = professorRepository.findAll();	
-			
-			System.out.println(professors);
+
+	@Autowired
+	ProfessorRepository professorRepository;
+
+	@Test
+	public void findAll() {
+		List<Professor> professors = professorRepository.findAll();
+		System.out.println(professors);
 	}
-		
-		@Test 
-		 void findById() {
-			
-		}
-	
+
+	@Test
+	void findById() {
+		Long id = 1l;
+		Optional<Professor> professors = professorRepository.findById(id);
+		Professor prf = professors.orElse(null);
+		System.out.println(prf);
+	}
+
+	@Test
+	void create() {
+		Professor prof1 = new Professor();
+		prof1.setName("Test");
+		prof1.setId(null);
+		Professor prof2 = professorRepository.save(prof1);
+		System.out.println(prof2);
+	}
+
+	@Test
+	void update() {
+		Professor prof1 = new Professor();
+		prof1.setName("Test2");
+		prof1.setId(1l);
+		Professor prof2 = professorRepository.save(prof1);
+		System.out.println(prof2);
+	}
+
+	@Test
+	void deleteById() {
+		Long id = 1l;
+		professorRepository.deleteById(id);
+	}
+
+	@Test
+	void deleteAll() {
+		professorRepository.deleteAllInBatch();
+	}
+
 }
