c4c0359d185ab41a8131578bf251933230397af0
Daniel Tompson <danieltompsonv@gmail.com>
2023-07-17 19:02:38 -0300

    Add @Test in DepartmentRepositoryTest

--- src/test/java/com/project/professor/allocation/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/project/professor/allocation/repository/DepartmentRepositoryTest.java
@@ -1,7 +1,8 @@
 package com.project.professor.allocation.repository;
 
+import java.util.List;
+import java.util.Optional;
 
-import com.project.professor.allocation.entity.Department;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,53 +11,57 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 
-import java.util.List;
-import java.util.Optional;
+import com.project.professor.allocation.entity.Department;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 @TestPropertySource(locations = "classpath:application.properties")
 public class DepartmentRepositoryTest {
-	
+
 	@Autowired
 	DepartmentRepository departmentRepository;
-	
+
 	@Test
 	public void findAll() {
 		List<Department> departments = departmentRepository.findAll();
-		
+
 		System.out.println(departments);
 	}
-	
+
+	@Test
 	public void findById() {
 		Long id = 1L;
 		Optional<Department> department = departmentRepository.findById(id);
 //		Department department = departmentRepository.findById(id).orElse(null);
-		
+
 		System.out.println(department);
 
 	}
 
+	@Test
 	public void create() {
 		Department department = new Department();
 		department.setId(null);
-		department.setName("Departmento 1");	
-		
+		department.setName("Departmento 1");
+
 		System.out.println(department);
 	}
-	
+
+	@Test
 	public void update() {
 		Department department = new Department();
 		department.setId(null);
 		department.setName("Departmento 2");
 	}
-	
+
+	@Test
 	public void deleteById() {
 		Long id = 1L;
 		departmentRepository.deleteById(id);
 	}
-	
+
+	@Test
 	public void deleteAll() {
 		departmentRepository.deleteAll();
 	}
